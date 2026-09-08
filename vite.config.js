f894a576f2c7f73643e78d40300695b04ebe515a
Mdonica <150047139+Mdonica@users.noreply.github.com>
2026-09-08 18:53:48 +0000

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/non-emergency-queue/',
+  base: '/codespaces-react/',
   plugins: [react()],
   test: {
     globals: true,
