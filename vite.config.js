5354a18952b94f31a303334be46fc33d0fa2a12b
Mdonica <150047139+Mdonica@users.noreply.github.com>
2026-09-08 19:08:12 +0000

    Deploy app under Prototype-non-e

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/codespaces-react/',
+  base: '/Prototype-non-e/',
   plugins: [react()],
   test: {
     globals: true,
